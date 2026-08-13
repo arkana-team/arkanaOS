b06df9f33e293497d275fa933b2a92feac147278
Debarka Kundu <debarkak@murena.io>
2026-08-13 23:38:12 +0200

    arkanas/05_graphics.mk: Set gio_sniffing=false in gdk-pixbuf meson setup to disable shared-mime-info dependency

--- arkanas/05_graphics.mk
+++ arkanas/05_graphics.mk
@@ -784,7 +784,7 @@ download-gdk-pixbuf: .gdk-pixbuf-obtained
 # Compile gdk-pixbuf
 gdk-pixbuf: download-gdk-pixbuf .gdk-pixbuf-done
 .gdk-pixbuf-done:
-	mkdir -p $(GDK_PIXBUF_PATH)/build && cd $(GDK_PIXBUF_PATH)/build && meson setup --native-file $(SRC_PATH)/cross_file.txt .. --prefix=/usr --buildtype=release -D others=enabled -D png=enabled -D shared_mime_info=disabled --wrap-mode=nofallback && \
+	mkdir -p $(GDK_PIXBUF_PATH)/build && cd $(GDK_PIXBUF_PATH)/build && meson setup --native-file $(SRC_PATH)/cross_file.txt .. --prefix=/usr --buildtype=release -D others=enabled -D png=enabled -D gio_sniffing=false --wrap-mode=nofallback && \
 	ninja && DESTDIR=$(STAGING_PATH) ninja install
 	touch .gdk-pixbuf-done
 
