9193dcc2886c2916052947e605fabad92a062273
Debarka Kundu <debarkak@murena.io>
2026-08-13 23:40:50 +0200

    arkanas/05_graphics.mk: Add Harfbuzz variables

--- arkanas/05_graphics.mk
+++ arkanas/05_graphics.mk
@@ -177,6 +177,10 @@ LIBRSVG_PATH = $(SRC_PATH)/librsvg-$(LIBRSVG_VER)
 LIBDAV1D_URL = https://sources.buildroot.net/dav1d/dav1d-1.5.1.tar.xz
 LIBDAV1D_VER = 1.5.1
 LIBDAV1D_PATH = $(SRC_PATH)/dav1d-$(LIBDAV1D_VER)
+# Harfbuzz
+HARFBUZZ_URL = https://github.com/harfbuzz/harfbuzz/releases/download/10.4.0/harfbuzz-10.4.0.tar.xz
+HARFBUZZ_VER = 10.4.0
+HARFBUZZ_PATH = $(SRC_PATH)/harfbuzz-$(HARFBUZZ_VER)
 
 # Cairo
 CAIRO_URL = https://www.cairographics.org/releases/cairo-1.18.2.tar.xz
