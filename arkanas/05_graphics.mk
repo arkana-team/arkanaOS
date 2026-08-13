260ae6a530476d8bb334e97fb98615f9443adbd3
Debarka Kundu <debarkak@murena.io>
2026-08-13 22:56:04 +0200

    arkanas/05_graphics.mk: Add touch .xbitmaps-obtained line

--- arkanas/05_graphics.mk
+++ arkanas/05_graphics.mk
@@ -287,6 +287,7 @@ XBITMAPS_PATH = $(SRC_PATH)/xbitmaps-$(XBITMAPS_VER)
 download-xbitmaps: .xbitmaps-obtained
 .xbitmaps-obtained:
 	cd $(SRC_PATH) && wget --tries=5 --timeout=30 -O xbitmaps-$(XBITMAPS_VER).tar.gz $(XBITMAPS_URL) && tar xf xbitmaps-$(XBITMAPS_VER).tar.gz
+	touch .xbitmaps-obtained
 FONT_UTIL_URL = https://gitlab.freedesktop.org/xorg/font/util/-/archive/XORG-STABLE/util-XORG-STABLE.tar.gz
 FONT_UTIL_VER = XORG-STABLE
 FONT_UTIL_PATH = $(SRC_PATH)/util-XORG-STABLE
