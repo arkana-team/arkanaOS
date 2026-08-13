db924bf58d8896b8b0bd3ec80a3f71a37343a08d
Debarka Kundu <debarkak@murena.io>
2026-08-13 20:24:17 +0200

    arkanas/05_graphics.mk: Add fontconfig download shell recipe line

--- arkanas/05_graphics.mk
+++ arkanas/05_graphics.mk
@@ -275,6 +275,7 @@ FONTCONFIG_VER = 2.17.1
 FONTCONFIG_PATH = $(SRC_PATH)/fontconfig-$(FONTCONFIG_VER)
 download-fontconfig: .fontconfig-obtained
 .fontconfig-obtained:
+	cd $(SRC_PATH) && wget --tries=5 --timeout=30 -O fontconfig-$(FONTCONFIG_VER).tar.xz $(FONTCONFIG_URL) && tar xf fontconfig-$(FONTCONFIG_VER).tar.xz
 FONT_UTIL_URL = https://gitlab.freedesktop.org/xorg/font/util/-/archive/XORG-STABLE/util-XORG-STABLE.tar.gz
 FONT_UTIL_VER = XORG-STABLE
 FONT_UTIL_PATH = $(SRC_PATH)/util-XORG-STABLE
