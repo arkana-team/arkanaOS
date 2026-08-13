6e6f45497167f1741bea56c36ef11c12ccfd77be
Debarka Kundu <debarkak@murena.io>
2026-08-13 20:24:04 +0200

    arkanas/05_graphics.mk: Add FONTCONFIG_VER variable

--- arkanas/05_graphics.mk
+++ arkanas/05_graphics.mk
@@ -271,6 +271,7 @@ freetype: download-freetype .freetype-done
 	$(MAKE) DESTDIR=$(STAGING_PATH) install
 	touch .freetype-done
 FONTCONFIG_URL = https://gitlab.freedesktop.org/api/v4/projects/890/packages/generic/fontconfig/2.17.1/fontconfig-2.17.1.tar.xz
+FONTCONFIG_VER = 2.17.1
 FONT_UTIL_URL = https://gitlab.freedesktop.org/xorg/font/util/-/archive/XORG-STABLE/util-XORG-STABLE.tar.gz
 FONT_UTIL_VER = XORG-STABLE
 FONT_UTIL_PATH = $(SRC_PATH)/util-XORG-STABLE
