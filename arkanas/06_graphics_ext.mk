46e9ab1898d5912fb0295fb0fa69115a762c2e56
Debarka Kundu <debarkak@murena.io>
2026-08-14 00:04:09 +0200

    arkanas/06_graphics_ext.mk: Add -lpng16 to weston c_link_args

--- arkanas/06_graphics_ext.mk
+++ arkanas/06_graphics_ext.mk
@@ -139,7 +139,7 @@ download-weston: .weston-obtained
 weston: download-weston libxkbcommon seatd .weston-done
 .weston-done:
     # Why are you using outdated FFmpeg syntax?
-	mkdir -p $(WESTON_PATH)/build && cd $(WESTON_PATH)/build && meson setup --native-file $(SRC_PATH)/cross_file.txt .. --prefix=/usr --buildtype=release -Ddemo-clients=false -Dbackend-vnc=false -Dcolor-management-lcms=false -Dimage-webp=false -Dpipewire=false -Dbackend-pipewire=false -Dbackend-rdp=false -Dremoting=false -Dtests=false -Dshell-kiosk=false -Dshell-fullscreen=false -Dbackend-drm-screencast-vaapi=false -Dc_link_args="-Wl,-rpath-link=$(STAGING_PATH)/usr/lib -lglib-2.0 -lgobject-2.0" && \
+	mkdir -p $(WESTON_PATH)/build && cd $(WESTON_PATH)/build && meson setup --native-file $(SRC_PATH)/cross_file.txt .. --prefix=/usr --buildtype=release -Ddemo-clients=false -Dbackend-vnc=false -Dcolor-management-lcms=false -Dimage-webp=false -Dpipewire=false -Dbackend-pipewire=false -Dbackend-rdp=false -Dremoting=false -Dtests=false -Dshell-kiosk=false -Dshell-fullscreen=false -Dbackend-drm-screencast-vaapi=false -Dc_link_args="-Wl,-rpath-link=$(STAGING_PATH)/usr/lib -lglib-2.0 -lgobject-2.0 -lpng16" && \
 	DESTDIR=$(STAGING_PATH) ninja install
 	touch .weston-done
 
